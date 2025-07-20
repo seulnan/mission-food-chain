09b1e4d8f32086a591070bac7c7b135ac5ffc2b1
kim nan seul <154604350+seulnan@users.noreply.github.com>
2025-07-21 02:03:30 +0900

    test: TDD를 위한 통합테스트코드 출력

--- src/test/java/mission/ApplicationTest.java
+++ src/test/java/mission/ApplicationTest.java
@@ -1,6 +1,5 @@
 package mission;
 
-import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import api.TestEnvironment;
@@ -18,6 +17,30 @@ public class ApplicationTest extends TestEnvironment {
     void 예외발생테스트() {
         run(List.of("잘못된입력"));
         assertTrue(output().contains("입력 형식이 올바르지 않습니다."));
+    }
+
+    @Test
+    void 빈문자열_입력시_예외메시지를_출력한다() {
+        run(List.of("   "));
+        assertTrue(output().contains("입력이 비어있습니다."));
+    }
+
+    @Test
+    void 동일한_물고기타입이_중복되면_예외메시지를_출력한다() {
+        run(List.of("[고등어-2],[고등어-3]"));
+        assertTrue(output().contains("중복된 물고기 타입이 존재합니다."));
+    }
+
+    @Test
+    void 음수수량입력시_예외메시지를_출력한다() {
+        run(List.of("[고등어--3]"));
+        assertTrue(output().contains("수량은 음수가 될 수 없습니다."));
+    }
+
+    @Test
+    void 아무_먹이가_없어_포식자가_죽는_경우_출력한다() {
+        run(List.of("[고등어-5]"));  // 플랑크톤 없음
+        assertTrue(output().contains("0일간 생존했습니다."));
     }
 
     @Override
